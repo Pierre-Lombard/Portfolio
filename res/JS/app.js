0c6f8d419210e4744cb9636028f58070d52b8266
Pierre <pierre.lombard9@gmail.com>
2023-02-26 08:27:54 +0200

    fixed weather feature

--- res/JS/app.js
+++ res/JS/app.js
@@ -2,16 +2,26 @@ function contact(){
     window.location.replace("mailto:pierre.lombard9@gmail.com");
 }
 
-const apiKey = 'Ye10e6b3ae76d342bcb9ae4a6c62e3b75';
-const city = 'Pretoria';
+const apiKey = 'e10e6b3ae76d342bcb9ae4a6c62e3b75';
+let city = 'Pretoria';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
+function getWeather(){
 fetch(url)
   .then(response => response.json())
   .then(data => {
     const weather = data.weather[0].description;
-    const temperature = Math.round(data.main.temp - 273.15); // Convert temperature from Kelvin to Celsius
+    const temperature = Math.round(data.main.temp - 273.15);
     console.log(`The weather in ${city} is ${weather} with a temperature of ${temperature}°C.`);
+    document.getElementById("weather").innerHTML = city + ', ' + temperature + '°C';
   })
   .catch(error => console.error(error));
+}
+
+function getLocation(){
+  city = window.prompt("Please enter your city:");
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  getWeather();
+}
+
+getWeather();
